c804127f04ce41fa62bff4a6b3c0eae9f4a7ddef
Jon Mellman <jonmellman@gmail.com>
2019-08-13 17:40:34 -0700

    Get same features as pycatfd

--- src/human_feature_detector.py
+++ src/human_feature_detector.py
@@ -2,6 +2,8 @@ import dlib
 import cv2
 from imutils import face_utils
 import matplotlib.pyplot as plt
+from collections import OrderedDict
+import numpy as np
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
@@ -9,14 +11,41 @@ predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
 image = cv2.imread('../human1.jpg')
 rects = detector(image, 1)
 
+# define a dictionary that maps the indexes of the facial
+# landmarks to specific face regions
+FACIAL_LANDMARKS_IDXS = OrderedDict([
+	("mouth", (48, 68)),
+	("right_eyebrow", (17, 22)),
+	("left_eyebrow", (22, 27)),
+	("right_eye", (36, 42)),
+	("left_eye", (42, 48)),
+	("nose", (27, 35)),
+	("jaw", (0, 17))
+])
+
+# Cat features are
+# chin, eyes, nose
+
+FEATURES = ['jaw', 'left_eye', 'right_eye', 'nose']
+
+def compute_feature_averages(shape):
+	return np.concatenate([
+		np.mean(shape[slice(*FACIAL_LANDMARKS_IDXS[feature])], axis=0, keepdims=True)
+		for feature in FEATURES	
+	], axis=0).astype(np.int32)
+	
+
+
+
+
 # loop over the face detections
 for (i, rect) in enumerate(rects):
 	# determine the facial landmarks for the face region, then
 	# convert the facial landmark (x, y)-coordinates to a NumPy
-	# array
+	# arraya
 	shape = predictor(image, rect)
 	shape = face_utils.shape_to_np(shape)
- 
+
 	# convert dlib's rectangle to a OpenCV-style bounding box
 	# [i.e., (x, y, w, h)], then draw the face bounding box
 	(x, y, w, h) = face_utils.rect_to_bb(rect)
@@ -30,6 +59,11 @@ for (i, rect) in enumerate(rects):
 	# and draw them on the image
 	for (x, y) in shape:
 		cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+
+	shape = compute_feature_averages(shape)
+
+	for (x, y) in shape:
+		cv2.circle(image, (x, y), 5, (255, 0, 255), -1)
  
 # show the output image with the face detections + facial landmarks
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
